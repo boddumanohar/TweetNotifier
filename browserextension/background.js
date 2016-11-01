68de2a038048cefe474611ef069b2df712551aff
boddu-manohar <201451024@iiitvadodara.ac.in>
2016-11-01 09:43:59 +0530

    previously implimneting API in node. Asked to change to Python.So changing to Python

---
+++ browserextension/background.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(function(object) {
+
+    if (localStorage.firsttime != 'false' || localStorage.firsttime == 'undefined' || !localStorage.firsttime) {
+        var optionsurl = chrome.extension.getURL("options.html");
+        chrome.tabs.create({
+            url: optionsurl
+        }, function(tab) {});
+
+        localStorage.firsttime = false;
+    }
+
+    var currentversion = chrome.app.getDetails().version;
+
+    if (!localStorage.version || localStorage.version != currentversion) {
+        chrome.browserAction.setBadgeText({
+            text: "NEW"
+        });
+        localStorage.version = currentversion
+
+    }
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
+     chrome.tabs.create({url: "https://www.google.co.in/#q=pnr+status"});
+});
