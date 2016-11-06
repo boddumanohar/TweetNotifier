81a78315d436cc9000079a1dd8a8c03095033c1a
boddu-manohar <201451024@iiitvadodara.ac.in>
2016-11-06 10:51:39 +0530

    added feature to add new handle

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
 
-
-
+# # from models import User, Handle
+# import models
+# # import models.Handle
 
 
 
@@ -26,54 +27,54 @@ timestamp = datetime.now().replace(minute = 0)
 
 # setting up Config files 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:gatsby@localhost/flaskmovie'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://manoharreddy@localhost/taskcprecog'
 
 # setting up DB 
-# 
-# db = SQLAlchemy(app)
 
-# class Users(db.Model):
-# 	__tablename__ = 'results'
+db = SQLAlchemy(app)
 
-# 	id = db.Column(db.Integer, primary_key = True)
-# 	user_id = db.Column(db.Integer, unique = True)
-# 	lastFetchedTweetId = db.Column(db.Integer, unique= True)
-# 	handles = db.relationship('Handle', backref='owner', lazy='dynamic')
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	user_id = db.Column(db.BigInteger, unique = True)
+	lastFetchedTweetId = db.Column(db.BigInteger(), unique= True)
+	handles = db.relationship('Handle', backref='owner', lazy='dynamic')
 
 
-# 	def __init__(self, user_id, handles):
-# 		self.user_id = user_id
-# 		self.handles = handles
+	# def __init__(self, user_id, handles):
+	# 	self.user_id = user_id
+	# 	self.handles = handles
 
 
-# 	def __repr__(self):
-# 		return 'users %r' %self.user_id	
+	# def __repr__(self):
+	# 	return 'users %r' %self.user_id	
 
-# class Handle(db.Model):
+class Handle(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	handle_name = db.Column(db.String(20))
+	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-# 		id = db.Column(db.Integer, primary_key = True)
-# 		handle_name = db.Column(db.String(20))
-# 		user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-# 		def __init__(self, handle_name, owner):
-# 			self.handle_name = handle_name
-# 			self.owner = owner
+		# def __init__(self, handle_name, owner):
+		# 	self.handle_name = handle_name
+		# 	self.owner = owner
 
 # setting up routes 
 # gets user_id by default 
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	counter = 0;
 	myvar = 0
-	lastFetchedTweetId = 791483941886697500
-	handle_name = 'aviayan123'
-	# lastFetchedTweetId = Users.query.filter_by(lastFetchedTweetId).first()
+
+	# lastFetchedTweetId = 791483941886697500
+	# handle_name = 'aviaryan123'
+
+	# lastFetchedTweetId = User.query.filter_by(lastFetchedTweetId).first()
 	# lastFetchedTweetId = lastFetchedTweetId.Users.all()
 
 	# user_id = request.form['user_id']
-	# r = Users.query.filter_by(user_id).first()
-	# for handle_name in r.handles.all():
+	r = Users.query.filter_by(user_id=123456).first()
+	for handle_name in r.handles.all():
 		itpTweets = twitter.statuses.user_timeline(screen_name=handle_name, since_id=lastFetchedTweetId)
 		r = {}
 		for t in itpTweets:
@@ -83,24 +84,33 @@ def main():
 			else:	 
 				counter += 1
 
-	# # update the table with the latestTweetID
+	# update the table with the latestTweetID
 	# latestTweetID = myvar
 	# stmt = update(Users).where(users.user_id==user_id).\
  #        values(lastFetchedTweetId=latestTweetID)
 
-	return  jsonify({'tweets': counter })
+	return  jsonify({"tweets":counter})
 
 
 @app.route('/login/', methods=['GET','POST'])
 def login():
 	return render_template('login.html')
 
-# @app.route('/postHandle', methods=['POST'])
-# def addHandle():
-# 	myhandle = Handle(request.form['handle'],request.form['user_id'])
-# 	db.session.add(myhandle)
-# 	db.session.commit()
-# 	return redirect(url_for('main'))		
+@app.route('/addhandle', methods=['GET', 'POST'])
+def addinghandle():
+	return render_template('addhandle.html')	
+
+@app.route('/postHandle', methods=['POST'])
+def addHandle():
+	# db.session.delete(models.User.query.filter_by(user_id=request.form['user_id']).first())
+	myowner = User.query.filter_by(user_id=request.form['user_id']).first()
+	new_handle = request.form['handle']
+	myhandle = Handle(handle_name=new_handle, owner = myowner)
+	db.session.add(myhandle)
+	db.session.commit()
+	return redirect(url_for('addinghandle'))
+
+			
     
 # --------- Server On ----------
 # start the webserver
