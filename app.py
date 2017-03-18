b8a4e25c0c3a5ea7054325d104d3e6fcc4d34051
boddumanohar <201451024@iiitvadodara.ac.in>
2017-03-18 21:17:43 +0530

    deploy 2 heroku second time

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)   # create our flask app
 db = SQLAlchemy(app)
 
 # configure Twitter API
-
 CONSUMER_KEY='WOWfe9QRArl5beDczAMfRP4jU'
 CONSUMER_SECRET='XBYI52DvlWNBzqMqpfzk98gxQLwFqUKeNpgJp1qTvIbfMv9hIH'
 OAUTH_TOKEN='560955859-MCmIMQ6k5UgVvAxA6fXj7MKBisWmTuUibzpGRI9m'
@@ -24,29 +23,9 @@ twitter = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_S
 timestamp = datetime.now().replace(minute = 0)
 
 # setting up Config files 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://manoharreddy@localhost/taskcprecog2'
-# app.config['SOCIAL_FACEBOOK'] = {
-#     'consumer_key': '131288720680435',
-#     'consumer_secret': '2811e08bbd7c648791dc238bf3fdbb16'
-# }
- 
-# app.config['SECURITY_POST_LOGIN'] = '/profile'
-
-# class Connection(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#     provider_id = db.Column(db.String(255))
-#     provider_user_id = db.Column(db.String(255))
-#     access_token = db.Column(db.String(255))
-#     secret = db.Column(db.String(255))
-#     display_name = db.Column(db.String(255))
-#     profile_url = db.Column(db.String(512))
-#     image_url = db.Column(db.String(512))
-#     rank = db.Column(db.Integer)
-
-
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
@@ -59,29 +38,10 @@ class Handle(db.Model):
 	lastFetchedTweetId = db.Column(db.String(), unique= True)
 	owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
-# Security(app, SQLAlchemyUserDatastore(db, User, Handle))
-# Social(app, SQLAlchemyConnectionDatastore(db, Connection))	
-
-# @app.route('/profile')
-# @login_required
-# def profile():
-#     return render_template(
-#         'profile.html',
-#         content='Profile Page',
-#         facebook_conn=social.facebook.get_connection())
-
-
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	counter = 0;
 	myvar = 0
-# since_id=handle.lastFetchedTweetId
-	# lastFetchedTweetId = 791483941886697500
-	# handle_name = 'aviaryan123'
-
-	# lastFetchedTweetId = User.query.filter_by(lastFetchedTweetId).first()
-	# lastFetchedTweetId = lastFetchedTweetId.Users.all()
 	empList = []
 	empDict = {}
 	handlesList = []
@@ -104,14 +64,7 @@ def main():
 		if(counter > 0):
 			Handle.query.filter_by(handle_name=i.handle_name).update({"lastFetchedTweetId":latesttweet})
 			db.session.commit()
-	# update the table with the latestTweetID
-	# latestTweetID = myvar
-	# stmt = update(Users).where(users.user_id==user_id).\
- #        values(lastFetchedTweetId=latestTweetID)
-
 	return jsonify(empList)
-	# return json.dumps(empList)
-
 
 @app.route('/login/', methods=['GET','POST'])
 def login():
@@ -123,7 +76,6 @@ def addinghandle():
 
 @app.route('/postHandle', methods=['POST'])
 def addHandle():
-	# db.session.delete(models.User.query.filter_by(user_id=request.form['user_id']).first())
 	latestTweetID = 0
 	myowner = User.query.filter_by(user_id=request.form['user_id']).first()
 	new_handle = request.form['handle']
--- browserextension/README.md
+++
@@ -1,3 +0,0 @@
-# Browser Extension
-
-To test it out, go to `chrome://extensions` and load this project's directory. 
